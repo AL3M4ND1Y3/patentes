89a218abaf715f360f5d0f96faa9dc88060b1d0f
AL3M4ND1Y3 <hugosebastianvillalva@gmail.com>
2023-09-10 16:19:53 -0300

    Actualizado

--- database.py
+++ database.py
@@ -2,28 +2,30 @@ import mysql.connector as db
 import json
 from datetime import datetime
 
-
-
 def subir_basededatos(foto_patente, license_plate):
-
     global nuevo_valor
     nuevo_valor = 0
-    with open('database.json') as database_file:
+    with open('config.json') as database_file:
         keys = json.load(database_file)
 
     # Crear conexión a la base de datos
     conex = db.connect(
-        host=keys["host"],
+        host=keys["host"],  
         user=keys["user"],
         password=keys["password"],
         database=keys["database"],
         port=keys["port"]
     )
+
+    parqueadero = keys["nombre"]
     cursor = conex.cursor()
-    query = "SELECT capacidad FROM parqueaderos WHERE id = 1 "
-    cursor.execute(query)
+    query = "SELECT id,espacios_ocupados FROM parqueaderos WHERE nombre = %s;"
+    cursor.execute(query, (parqueadero,))
     result1 = cursor.fetchone()
-    espacio = result1[0]
+    print(result1)
+    parqueadero_id = result1[0]
+    espacio = result1[1]
+    print(espacio)
 
     # Verificar si la placa ya está registrada en la tabla "vehiculos"
     query = "SELECT * FROM vehiculos WHERE placa = %s"
@@ -44,31 +46,31 @@ def subir_basededatos(foto_patente, license_plate):
             if hora_entrada and hora_salida:
                 # Si el vehículo ya tiene entrada y salida, se agrega una nueva fila con una nueva entrada
                 fecha_hora = datetime.now()
-                query = "INSERT INTO registros_entradas_salidas (vehiculo_id, parqueadero_id, espacio_estacionamiento_id, fecha_entrada, foto_entrada, precio) VALUES (%s, 1, 1, %s, %s, 1000)"
-                cursor.execute(query, (vehiculo_id, fecha_hora, foto_patente))
+                query = "INSERT INTO registros_entradas_salidas (vehiculo_id, parqueadero_id, espacio_estacionamiento_id, fecha_entrada, foto_entrada, precio) VALUES (%s, %s, 1, %s, %s, 0)"
+                cursor.execute(query, (vehiculo_id, parqueadero_id ,fecha_hora, foto_patente))
                 conex.commit()
-                nuevo_valor = espacio - 1
+                nuevo_valor = espacio + 1
             elif hora_entrada and not hora_salida:
                 # Si el vehículo ya tiene entrada y no tiene salida, se agrega como salida con la foto correspondiente
                 fecha_hora = datetime.now()
                 query = "UPDATE registros_entradas_salidas SET fecha_salida = %s WHERE vehiculo_id = %s AND fecha_entrada = %s"
                 cursor.execute(query, (fecha_hora, vehiculo_id, hora_entrada))
                 conex.commit()
-                nuevo_valor = espacio + 1
+                nuevo_valor = espacio - 1
             else:
                 # Si el vehículo no tiene entrada o ya tiene entrada y salida, se agrega como entrada
                 fecha_hora = datetime.now()
-                query = "INSERT INTO registros_entradas_salidas (vehiculo_id, parqueadero_id, espacio_estacionamiento_id, fecha_entrada, foto_entrada, precio) VALUES (%s, 1, 1, %s, %s, 1000)"
-                cursor.execute(query, (vehiculo_id, fecha_hora, foto_patente))
+                query = "INSERT INTO registros_entradas_salidas (vehiculo_id, parqueadero_id, espacio_estacionamiento_id, fecha_entrada, foto_entrada, precio) VALUES (%s, %s, 1, %s, %s, 0)"
+                cursor.execute(query, (vehiculo_id, parqueadero_id,fecha_hora, foto_patente))
                 conex.commit()
-                nuevo_valor = espacio - 1
+                nuevo_valor = espacio + 1
         else:
             # El vehículo no tiene entradas ni salidas registradas, se agrega como entrada
             fecha_hora = datetime.now()
-            query = "INSERT INTO registros_entradas_salidas (vehiculo_id, parqueadero_id, espacio_estacionamiento_id, fecha_entrada, foto_entrada, precio) VALUES (%s, 1, 1, %s, %s, 1000)"
-            cursor.execute(query, (vehiculo_id, fecha_hora, foto_patente))
+            query = "INSERT INTO registros_entradas_salidas (vehiculo_id, parqueadero_id, espacio_estacionamiento_id, fecha_entrada, foto_entrada, precio) VALUES (%s, %s, 1, %s, %s, 0)"
+            cursor.execute(query, (vehiculo_id, parqueadero_id ,fecha_hora, foto_patente))
             conex.commit()
-            nuevo_valor = espacio - 1
+            nuevo_valor = espacio + 1
     else:
         # La placa no existe en la tabla "vehiculos"
         fecha_hora = datetime.now()
@@ -78,14 +80,14 @@ def subir_basededatos(foto_patente, license_plate):
         conex.commit()
 
         # Insertar en la tabla "registros_entradas_salidas" con el ID del vehículo recién insertado
-        query = "INSERT INTO registros_entradas_salidas (vehiculo_id, parqueadero_id, espacio_estacionamiento_id, fecha_entrada, foto_entrada, precio) VALUES (%s, 1, 1, %s, %s, 1000)"
-        cursor.execute(query, (vehiculo_id, fecha_hora, foto_patente))
+        query = "INSERT INTO registros_entradas_salidas (vehiculo_id, parqueadero_id, espacio_estacionamiento_id, fecha_entrada, foto_entrada, precio) VALUES (%s, %s, 1, %s, %s, 0)"
+        cursor.execute(query, (vehiculo_id, parqueadero_id,fecha_hora, foto_patente))
         conex.commit()
-        nuevo_valor = espacio - 1
+        nuevo_valor = espacio + 1
 
     print(nuevo_valor)
-    query = "UPDATE parqueaderos SET capacidad = %s WHERE id = 1"
-    cursor.execute(query, (nuevo_valor,))
+    query = "UPDATE parqueaderos SET espacios_ocupados = %s WHERE  nombre = %s"
+    cursor.execute(query, (nuevo_valor,parqueadero))
     conex.commit() 
 
     # Cerrar conexión a la base de datos
---
+++ main.py
@@ -0,0 +1,125 @@
+########### Dependencies ##############
+import tkinter as tk
+import cv2
+from sys import exit as exit
+import time
+import requests
+import os
+import uuid
+import tempfile
+from database import subir_basededatos  
+import json
+
+config_file = 'config.json'
+
+if os.path.exists(config_file):
+    with open(config_file, 'r') as f:   
+        config = json.load(f)
+else:
+    # Si el archivo de configuración no existe, solicita la configuración al usuario
+    config = {
+        "host" : "localhost",
+        "user" : "root",
+        "password" : "",
+        "database" : "applinkingparking",
+        "port" : 3307,
+        "nombre": input("Ingrese el nombre: "),
+    }
+
+    # Guarda la configuración en un archivo JSON
+    with open(config_file, 'w') as f:
+        json.dump(config, f)
+
+
+#funcion para leer la placa
+def leer_placa(img):
+    try:
+        regions = ['ar', 'it'] 
+        with open(img, 'rb') as fp:
+            response = requests.post(
+                'https://api.platerecognizer.com/v1/plate-reader/',
+                data=dict(regions=regions),
+                files=dict(upload=fp),
+                headers={'Authorization': 'Token b76c3cc286feaed0c6f30eecafaf72a183682e5d '})
+            data = response.json()
+        return data
+    except:
+        change_to_orange()
+
+
+def crear_interfaz():
+    global root, label
+
+    root = tk.Tk()
+    root.attributes('-fullscreen', True)
+
+
+    label = tk.Label(root, font=('Helvetica', 30), fg='white')
+    label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+""" 
+def switch_to_red():
+    root.configure(bg='red')
+    label.configure(text='PARE', bg='red', fg='white')
+"""
+def change_to_green():
+    root.configure(bg='green')
+    label.configure(text='PASE', bg='green', fg='white')
+    root.after(2000, root.destroy)
+
+    global patente_detectado
+    patente_detectado = False
+
+def change_to_orange():
+    root.configure(bg='orange')
+    label.configure(text='No se pudo leer la placa', bg='orange', fg='white')
+
+
+def on_press_enter(photo_path,plate):
+    data = leer_placa(photo_path)
+
+    my_uuid = str(uuid.uuid4())
+    print(my_uuid)
+
+    foto = my_uuid + "-" + plate + ".jpg"
+    ruta_guardado = r"C:\xampp\htdocs\phpPrueba\fotos"
+    if not os.path.exists(ruta_guardado):
+        os.makedirs(ruta_guardado)
+
+    ruta_foto = os.path.join(ruta_guardado, foto)
+    cv2.imwrite(ruta_foto, cv2.imread(photo_path))
+    if subir_basededatos(foto, plate):
+        crear_interfaz()
+        change_to_green()
+        root.mainloop()
+        global patente_detectado
+        patente_detectado = False
+
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+    patente_detectado = False
+    ultima_patente = None
+
+    while True:
+        ret, frame = cap.read()
+        with tempfile.NamedTemporaryFile(suffix='.jpg', delete=False) as temp_file:
+            cv2.imwrite(temp_file.name, frame)
+
+        data = leer_placa(temp_file.name)
+        print(data)
+
+        if data.get('results') and len(data['results']) > 0:
+            if ultima_patente != data['results'][0]['plate']:
+                patente_detectado = True
+                plate = data['results'][0]['plate']
+                ultima_patente = data['results'][0]['plate']
+                print(ultima_patente)
+
+                on_press_enter(temp_file.name, plate)  # Llamar a on_enter_press con la ID de cámara
+            else:
+                time.sleep(0.5)
+                ultima_patente = None
+
+    cap.release()
+    cv2.destroyAllWindows()
